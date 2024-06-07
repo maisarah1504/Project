9e793bacf03ebf23a50a1c63c62d800ad45816f7
foxgrim <rasyedlee@gmail.com>
2024-06-07 21:42:18 +0800

    rasyid

--- rasyid/availability.php
+++ rasyid/availability.php
@@ -6,7 +6,7 @@
     <title>FKPark - Parking Availability</title>
     <!-- Linking Google font link for icons -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200">
-    <link rel="stylesheet" href="base.css">
+    <link rel="stylesheet" href="styles.css">
     <style>
         body {
             display: flex;
